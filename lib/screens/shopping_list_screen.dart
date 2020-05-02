8205bd7ed35a4c9a62105bbacd188b3f149df6dd
mdolinin <dmo.builder@gmail.com>
2020-05-01 21:08:39 -0400

    Add basic Drawer with some future menu items.

--- lib/screens/shopping_list_screen.dart
+++ lib/screens/shopping_list_screen.dart
@@ -32,6 +32,43 @@ class _ShoppingListScreenState extends State<ShoppingListScreen>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+        child: Material(
+          color: Theme.of(context).primaryColor,
+          child: SafeArea(
+            child: Theme(
+              data: ThemeData(brightness: Brightness.dark),
+              child: ListView(
+                padding: EdgeInsets.zero,
+                children: [
+                  ListTile(
+                    leading: Text(
+                      'YAPA',
+                      style: Theme.of(context).textTheme.headline3,
+                    ),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.shopping_basket),
+                    title: Text('Shopping list'),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.view_list),
+                    title: Text('Catalog'),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.category),
+                    title: Text('Categories'),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.settings),
+                    title: Text('Settings'),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
       body: NestedScrollView(
         controller: _scrollViewController,
         headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
